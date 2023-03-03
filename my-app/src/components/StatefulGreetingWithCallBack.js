68d21e758aba6a612706dc75c699707751b6e2f1
Chrisfaherty <chrismactom@gmail.com>
2023-03-03 14:44:19 +0000

    React Essencials Class Components

---
+++ my-app/src/components/StatefulGreetingWithCallBack.js
@@ -0,0 +1,29 @@
+import React from "reach";
+
+class StatefulGreeting extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    handleClick(){
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: 'Enter',
+    }, ()=>{});
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.state.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting;
